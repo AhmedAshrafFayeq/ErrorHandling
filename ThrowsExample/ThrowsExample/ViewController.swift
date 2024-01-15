c8d8f4977b152cb7fa96777c01f9cc98061aa15a
Ahmed Fayeq <ahmedashraffayeq@gmail.com>
2024-01-15 15:37:17 +0300

    initial commit

---
+++ ThrowsExample/ThrowsExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ThrowsExample
+//
+//  Created by Ahmed Fayek on 15/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
