e1a261bdf3804c03d80b0273ac06c2b6f46ac516
Ahmed Fayeq <ahmedashraffayeq@gmail.com>
2024-01-16 10:06:30 +0300

    - Decode API response to list of users

---
+++ ThrowsExample/ThrowsExample/User.swift
@@ -0,0 +1,12 @@
+//
+//  User.swift
+//  ThrowsExample
+//
+//  Created by Ahmed Fayek on 16/01/2024.
+//
+
+import Foundation
+
+struct User: Codable {
+    let name: String
+}
--- ThrowsExample/ThrowsExample/ViewController.swift
+++ ThrowsExample/ThrowsExample/ViewController.swift
@@ -17,18 +17,30 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        do {
+            try fetchUsers(completion: { result in
+                switch result {
+                case .success(let users):
+                    print(users)
+                case .failure(let error):
+                    print(error)
+                }
+            })
+        }catch {
+            print(error)
+        }
     }
 
-    func fetchUsers(completion: @escaping (Result<String, Error>)-> Void) throws {
+    func fetchUsers(completion: @escaping (Result<[User], Error>)-> Void) throws {
         guard let url = URL(string: "https://jsonplaceholder.typicode.com/users") else {
             throw UserFetchError.invalidUrl
         }
         
-        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+        let _ = URLSession.shared.dataTask(with: url) { data, _, error in
             if let error {completion(.failure(error))}
             else if let data {
                 do {
-                    let result = try JSONDecoder().decode([User], from: data)
+                    let result = try JSONDecoder().decode([User].self, from: data)
                     completion(.success(result))
                 }catch {
                     completion(.failure(error))
